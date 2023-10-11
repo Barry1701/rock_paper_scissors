4dadfbe1d64d9adb16f78359305a7ceca686283d
Barry1701 <>
2023-10-11 14:19:35 +0000

    add part of js code provided from Portfolio Project 2 video

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+/** 
+Declare constants for DOM elements
+and possible choices*/
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const messages = document.getElementById("messages");
+const choices = ["rock", "paper", "scissors"];
+
+/** 
+Add Event Listeners to all the buttons*/
+
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
+}
+
+/** The main game function. Accept one parameter, 
+which is the data-choice value of the selected button*/
+function playGame(player-choice) {
+
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
+
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    playerImage.src = `assets/images/${choices[computerChoice]}.png`;
+    playerImage.alt = choices[computerChoice];
+
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+    updateScores(results);
+}
+/** 
+Check to see who the winner is*/
