62d3a02f8b18db74c92d50721a1c8459f7c20e17
Barry1701 <>
2023-10-12 07:32:41 +0000

    Add checkWinner function to determine who the winner is

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,27 +13,41 @@ const choices = ["rock", "paper", "scissors"];
 Add Event Listeners to all the buttons*/
 
 for (let button of buttons) {
-    button.addEventListener("click", function () {
-        let playerChoice = this.getAttribute("data-choice");
-        playGame(playerChoice);
-    });
+  button.addEventListener("click", function () {
+    let playerChoice = this.getAttribute("data-choice");
+    playGame(playerChoice);
+  });
 }
 
 /** The main game function. Accept one parameter, 
 which is the data-choice value of the selected button*/
-function playGame(player-choice) {
+function playGame(playerChoice) {
+  playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+  playerImage.alt = choices[playerChoice];
 
-    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
-    playerImage.alt = choices[playerChoice];
+  let computerChoice = Math.floor(Math.random() * 3);
 
-    let computerChoice = Math.floor(Math.random() * 3);
+  computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+  computerImage.alt = choices[computerChoice];
 
-    playerImage.src = `assets/images/${choices[computerChoice]}.png`;
-    playerImage.alt = choices[computerChoice];
+  let result = checkWinner(choices[computerChoice], choices[playerChoice]);
 
-    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
-
-    updateScores(results);
+  updateScores(result);
 }
 /** 
-Check to see who the winner is*/
+Check to see who the winner is.
+Determine the winner based on computer's and payer's choice */
+
+function checkWinner(computerChoice, playerChoice) {
+    if(computerChoice === playerChoice){
+        return "tie";
+    }else if(
+    (computerChoice === "rock" && playerChoice === "scissors")||
+    (computerChoice === "paper" && playerChoice === "rock")||
+    (computerChoice === "scissors" && playerChoice === "paper")
+    ) {
+        return "computer";
+    }else{
+        return "player";
+    }
+}
